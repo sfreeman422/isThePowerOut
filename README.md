a42dfc5d278370ebaa48372352fcda920cbcab67
steve <sfreeman422@protonmail.com>
2021-08-22 03:23:24 -0400

    Init commit

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # isThePowerOut
+
 Text Messages When The Power In Your House Goes Out
+
+## Getting Started
+
+Add the following environment variables:
+
+```
+to: The number you want to text.
+from: A valid Twilio phone number
+TWILIO_AUTH: A valid Twilio auth key.
+TWILIO_SID: A valid Twilio SID
+```
+
+Be sure to change the `url` to a local url on your network that will turn off should the power go off.
+
+`npm i`
+`npm run start`
---
+++ index.js
@@ -0,0 +1,39 @@
+const twilio = require("twilio")(
+  process.env.TWILIO_SID,
+  process.env.TWILIO_AUTH
+);
+const Ping = require("ping-wrapper");
+
+function main() {
+  Ping.configure();
+  const url = "192.168.1.93";
+  const ping = new Ping(url);
+
+  console.log("Pinging ", url, "\n You will only be alerted if a ping fails.");
+
+  let numberOfTexts = 0;
+
+  ping.on("fail", function (data) {
+    console.log("Failure", data);
+    if (numberOfTexts < 5) {
+      console.log("Alerting ", process.env.to);
+      twilio.messages
+        .create({
+          body: `ALERT: Your power has gone out.`,
+          to: process.env.to, // Text this number
+          from: process.env.from, // From a valid Twilio number
+        })
+        .then(() => {
+          numberOfTexts += 1;
+
+          if (numberOfTexts === 5) {
+            console.log("Max limit reached - will alert again in 30 seconds");
+            setTimeout(() => (numberOfTexts = 0), 30000);
+          }
+        })
+        .catch((e) => console.error(e));
+    }
+  });
+}
+
+main();
